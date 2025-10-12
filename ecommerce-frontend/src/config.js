fa4f64c4f6ad008387b6bf58e3e2554ce27f3ab9
thanhhuynh1609 <hnkt1609@gmail.com>
2025-10-12 20:58:12 +0700

    fix url call be

--- ecommerce-frontend/src/config.js
+++ ecommerce-frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_URL: window.location.hostname === 'localhost' 
     ? 'http://localhost:8080/api'
-    : 'https://icoud.onrender.com/api'
+    : 'http://13.239.246.186:3001/api'
 };
 
 export default config;
